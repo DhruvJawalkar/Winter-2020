7d85e5cc84284727c1047c8a3546488ff68cded8
Dhruv <jawalkad@oregonstate.edu>
2020-02-29 16:24:29 -0800

    Update 359-logger_rate_limiter.py

--- leetcode/359-logger_rate_limiter.py
+++ leetcode/359-logger_rate_limiter.py
@@ -31,3 +31,32 @@ class Logger:
             
             return False
             
+##############################################################################
+# sol 2
+
+class Logger:
+
+def __init__(self):
+    """
+    Initialize your data structure here.
+    """
+    self.messages = dict()
+
+def shouldPrintMessage(self, timestamp: int, message: str) -> bool:
+    """
+    Returns true if the message should be printed in the given timestamp, otherwise returns false.
+    If this method returns false, the message will not be printed.
+    The timestamp is in seconds granularity.
+    """
+    if(message not in self.messages):
+        self.messages[message] = timestamp
+        return True
+    else:
+        prev_timestamp = self.messages[message]
+        
+        if(timestamp-prev_timestamp>=10):
+            self.messages[message] = timestamp
+            return True
+        
+        return False
+            
