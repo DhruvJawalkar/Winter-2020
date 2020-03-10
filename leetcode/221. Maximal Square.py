6ef3749adae2f3e5c0f45828da428429eb4c38e8
Dhruv <jawalkad@oregonstate.edu>
2020-03-09 20:20:08 -0700

    updated 221. Maximal Square

--- leetcode/221. Maximal Square.py
+++ leetcode/221. Maximal Square.py
@@ -15,3 +15,28 @@ def maximalSquare(self, matrix: List[List[str]]) -> int:
     
     return max_square**2
                 
+##############################################################################
+
+class Solution:
+def maximalSquare(self, matrix: List[List[str]]) -> int:
+    if(matrix==[]):
+        return 0
+    
+    memory = [0 for j in range(len(matrix[0]))]
+    max_square = 0
+    prev = 0
+    
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0])):
+            temp = memory[j]
+            
+            if(i==0 or j==0 or int(matrix[i][j])==0):
+                memory[j] = int(matrix[i][j])
+            else:
+                memory[j] = min(memory[j-1], memory[j], prev)+1
+            
+            prev = temp
+            max_square = max(memory[j], max_square)
+    
+    return max_square**2
+                
